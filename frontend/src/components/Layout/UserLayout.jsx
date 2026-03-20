4ba44d8d2b64c3dc5f8a5d603f1c9b4e456e3b85
Dilsha Jayasekara <dilshajayasekara2024@gmail.com>
2026-03-20 11:31:35 +0530

    added hero section with shop now button

--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Header from '../Common/Header';
 import Footer from '../Common/Footer';
+import { Outlet } from 'react-router-dom';
 
 const UserLayout = () => {
   return (
     <>
     <Header/>
+
+    {/*Main content*/}
+    <main>
+      <Outlet/>
+    </main>
 
     <Footer/>
 
