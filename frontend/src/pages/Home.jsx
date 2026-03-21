035e8f4007b2cd3074019e3ff9243ea5f403c709
Dilsha Jayasekara <dilshajayasekara2024@gmail.com>
2026-03-21 20:52:54 +0530

    add New Arrivals component and fix scroll button logic

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Layout/Hero'
 import GenderCollectionSection from './GenderCollectionSection'
+import NewArrivals from '../components/Products/NewArrivals'
 
 const Home = () => {
   return (
     <div>
         <Hero/>
         <GenderCollectionSection/>
+        <NewArrivals/>
     </div>
   )
 }
