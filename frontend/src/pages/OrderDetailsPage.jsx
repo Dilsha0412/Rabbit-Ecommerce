382b5d80fd67a7171918a7192f2b5c47e2d0b343
Dilsha Jayasekara <dilshajayasekara2024@gmail.com>
2026-04-09 23:42:56 +0530

    initial development of OrderDetails page and UI layout

---
+++ frontend/src/pages/OrderDetailsPage.jsx
@@ -0,0 +1,64 @@
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+
+
+const OrderDetailsPage = () => {
+    const { id } = useParams();
+    const [orderDetails, setOrderDetails] = useState(null);
+    useEffect(() => {
+        const mockOrderDetails = {
+            _id: id,
+            createdAT: new Date(),
+            isPaid: true,
+            isDelivered: false,
+            paymentMethod: "PayPal",
+            shippingMethod:"Standard",
+            shippingAdddress: { city: "New York",country: "USA" },
+            orderItems:[
+                {
+                    productId: "1",
+                    name: "Jacket",
+                    price: 100,
+                    Quantity:1,
+                    image: "https://picsum.photos/150?random=1",
+                },
+
+                {
+                    productId: "2",
+                    name: "Shirt",
+                    price: 150,
+                    Quantity:2,
+                    image: "https://picsum.photos/150?random=2",
+                },
+            ],
+        };
+        setOrderDetails(mockOrderDetails);
+    }, [id]);
+    
+  return 
+    <div className='max-w-7xl mx-auto p-4 sm:p-6'>
+        <h2 className='text-2xl md:text-3xl font-bold mb-6'>Order Details</h2>
+        {!orderDetails ? (
+            <p>No Order Details Found</p>
+        ) : (
+            <div className='p-4 sm:p-6 rounded-lg border'>
+                {/* Order Info */}
+                <div className='flex flex-col sm:flex-row justify-between mb-8'>
+                    <div>
+                        <h3 className='text-lg md: text-xl font-semibold'>
+                            Order ID: #{orderDetails._id}
+                        </h3>
+                        <p className='text-gray-600'>
+                            {new Date(orderDetails.createdAT).toLocaleDateString()}
+                        </p>
+                    </div>
+                </div>
+                </div>
+               ) }
+
+    </div>
+  
+};
+
+export default OrderDetailsPage;
