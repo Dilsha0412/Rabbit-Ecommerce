168e13f87bf6329591bf32dab2d24fde1d6529f1
Dilsha Jayasekara <dilshajayasekara2024@gmail.com>
2026-04-10 23:46:33 +0530

    developing the OrderDetailsPage.jsx file

--- frontend/src/pages/OrderDetailsPage.jsx
+++ frontend/src/pages/OrderDetailsPage.jsx
@@ -46,19 +46,37 @@ const OrderDetailsPage = () => {
                 {/* Order Info */}
                 <div className='flex flex-col sm:flex-row justify-between mb-8'>
                     <div>
-                        <h3 className='text-lg md: text-xl font-semibold'>
+                        <h3 className='text-lg md:text-xl font-semibold'>
                             Order ID: #{orderDetails._id}
                         </h3>
                         <p className='text-gray-600'>
                             {new Date(orderDetails.createdAT).toLocaleDateString()}
                         </p>
                     </div>
-                </div>
+                    <div className='flex flex-col items-start sm:items-end mt-4 sm:mt-0'>
+                        <span className={`${
+                            orderDetails.isPaid 
+                            ? "bg-green-100 text-green-700" 
+                            : "bg-red-100 text-red-700"
+                            } px-3 py-1 rounded-full text-sm font-medium mb-2`}
+                            >
+                                {orderDetails.isPaid ? "Approved" : "Pending"}
+                        </span>
+
+                         <span className={`${
+                            orderDetails.isDelivered 
+                            ? "bg-green-100 text-green-700" 
+                            : "bg-yellow-100 text-yellow-700"
+                            } px-3 py-1 rounded-full text-sm font-medium mb-2`}
+                            >
+                                {orderDetails.isDelivered ? "Delivered" : "Not Delivered"}
+                        </span>
+                    </div>
+                   </div>
                 </div>
                ) }
-
     </div>
-  
+ 
 };
 
 export default OrderDetailsPage;
